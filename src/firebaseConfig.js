74aba617813308b3d8d7a68b0d28bad25e4dd739
Nikhil Verma <its-nikhil@hotmail.com>
2026-05-04 20:32:28 +0530

    UI

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBarml8DfdYetciBzKFoB4-MkwCEmhPyMI",
-  authDomain: "myauthapp-e9639.firebaseapp.com",
-  projectId: "myauthapp-e9639",
-  storageBucket: "myauthapp-e9639.firebasestorage.app",
-  messagingSenderId: "858615101749",
-  appId: "1:858615101749:web:07af4f7242363aa49a747c",
-  measurementId: "G-XE422NF9RN"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
